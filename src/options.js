00c6cf4ed95964e84c7e7807c2193e6c0cc6bfe0
Katsute <58778985+Katsute@users.noreply.github.com>
2024-01-28 21:31:19 -0500

    add options saving and loading

--- src/options.js
+++ src/options.js
@@ -4,62 +4,98 @@
 
     const identifier = "bkx";
 
-    // functions
-
-    const $ = document.querySelector;
-    const $$ = document.querySelectorAll;
-
     // save & load
 
     const load = (options) => {
         for(const type of ["window", "editor", "sidebar", "panel"]){
-            // TODO: load setting into HTML
+            const key = type.charAt(0);
 
-            // TODO: default & fallback values
+            // TODO: load images
+
+            for(const option of ["Position", "PositionManual", "Repeat", "Size", "Opacity", "Blur", "Time"]){
+                if(options[`${key}${option}`])
+                    document.querySelector(`#${identifier}-${key}${option}`).value = options[`${key}${option}`];
+            }
         };
 
-        $(`${identifier}-wA`).checked = options.renderContentAboveBackground;
-        $(`${identifier}-si`).checked = options.smoothImageRendering;
+        document.querySelector(`#${identifier}-renderContentAboveBackground`).checked = options.renderContentAboveBackground ?? false;
+        document.querySelector(`#${identifier}-smoothImageRendering`).checked = options.smoothImageRendering ?? false;
 
-        $(`${identifier}-css`).value = options.css;
+        document.querySelector(`#${identifier}-css`).value = options.css ?? '';
+
+        for(const option of document.querySelectorAll("option[value='custom']")){
+            const input = option.parentElement;
+            input.nextElementSibling.disabled = input.value !== "custom";
+        };
     };
 
     const save = async (options) => {
         await chrome.storage.sync.set(options);
     };
 
+    const reset = async () => await chrome.storage.sync.clear();
+
+    load(await chrome.storage.sync.get() ?? {});
+
     // change observer
 
-    for(const input of $$(["input", "select", "textarea"])){
+    for(const input of document.querySelectorAll(["input", "select", "textarea"])){
         input.addEventListener("change", () => {
-            save({
-                // TODO: save setting from HTML
-            });
+            const options = {
+                renderContentAboveBackground: document.querySelector(`#${identifier}-renderContentAboveBackground`).checked,
+                smoothImageRendering: document.querySelector(`#${identifier}-smoothImageRendering`).checked,
+                css: document.querySelector(`#${identifier}-css`).value
+            }
+
+            for(const type of ["window", "editor", "sidebar", "panel"]){
+                const key = type.charAt(0);
+
+                // TODO: save images
+
+                for(const option of ["Position", "PositionManual", "Repeat", "Size", "Opacity", "Blur", "Time"]){
+                    options[`${key}${option}`] = document.querySelector(`#${identifier}-${key}${option}`).value;
+                }
+            };
+
+            save(options);
         });
     };
 
-    for(const option of $$("option[value='custom']")){
+    for(const option of document.querySelectorAll("option[value='custom']")){
         const input = option.parentElement;
         input.addEventListener("change", () => {
-            input.nextSibling.disabled = input.value !== "custom";
+            input.nextElementSibling.disabled = input.value !== "custom";
         });
     };
 
     // import & export
 
-    $("#${identifier}-import").addEventListener("click", () => {
-        // TODO: import from json
+    document.querySelector(`#${identifier}-import`).addEventListener("click", () => document.querySelector(`#${identifier}-import-container`).click());
 
-        // TODO: dump invalid properties
-
-        // TODO: handle invalid types
+    document.querySelector(`#${identifier}-import-container`).addEventListener("change", (e) => {
+        if(e.target.files.length === 1){
+            const reader = new FileReader();
+            reader.onload = (c) => {
+                const content = c.target.result;
+                const options = JSON.parse(content);
+                save(options);
+                load(options);
+            };
+            reader.readAsText(e.target.files[0], "UTF-8");
+        }
     });
 
-    $("#${identifier}-export").addEventListener("click", () => {
-        // TODO: export to json from HTML
+    document.querySelector(`#${identifier}-export`).addEventListener("click", async () => {
+        const container = document.querySelector(`#${identifier}-export-container`);
+
+        container.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(await chrome.storage.sync.get() ?? {}, null, 4))}`);
+        container.setAttribute("download", "settings.json");
+        container.click();
     });
 
-    // init
-
-    load(await chrome.storage.sync.get("options"));
+    document.querySelector(`#${identifier}-reset`).addEventListener("click", () => {
+        reset().then(() => {
+            load({});
+        })
+    });
 })();
