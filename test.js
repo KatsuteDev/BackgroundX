94a92207ad23f87958715a7b93d575ab85a34250
Katsute <58778985+Katsute@users.noreply.github.com>
2024-07-07 22:12:35 -0400

    simplify tests

--- test.js
+++ test.js
@@ -1,48 +1,30 @@
-const assert = require("assert");
 const puppeteer = require("puppeteer");
 
+const URL = "https://vscode.dev";
+
+const selectors = [
+    `body div[role="application"] > div.monaco-grid-view`,
+    `.split-view-view > .editor-group-container`,
+    `.split-view-view > .part.sidebar`,
+    `.split-view-view > .part.auxiliarybar`,
+    `.split-view-view > .part.panel`,
+];
+
 (async () => {
-    const URL = "https://vscode.dev";
-
-    // init puppeteer
-
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(URL);
 
-    const selectorExists = async (selector) => {
-        try{
-            await page.waitForSelector(selector, { timeout: 5000 });
-            return true;
-        }catch(e){
-            return false;
-        }
-    };
-
-    // selectors
-
-    const selectors = [
-        `body div[role="application"] > div.monaco-grid-view`,
-        `.split-view-view > .editor-group-container`,
-        `.split-view-view > .part.sidebar`,
-        `.split-view-view > .part.auxiliarybar`,
-        `.split-view-view > .part.panel`,
-    ];
-
-    // test
-
     let failed = false;
     for(const s of selectors){
         try{
-            assert(await selectorExists(s));
+            await page.waitForSelector(s, { timeout: 5000 });
             console.info('✅', s);
         }catch(e){
             console.error('❌', s);
             failed = true;
         }
     }
-
-    // post
 
     await browser.close();
 
